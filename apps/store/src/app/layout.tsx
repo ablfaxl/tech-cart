381d7aa206551100237b8531eec2b9cebb350da9
Abolfazl Taghavi <taghavi.a2023@gmail.com>
2025-03-19 20:29:51 +0330

    chore: initial code on store

--- apps/store/src/app/layout.tsx
+++ apps/store/src/app/layout.tsx
@@ -12,7 +12,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
+        {children}
+      </body>
     </html>
   );
 }
