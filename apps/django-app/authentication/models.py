95e4a27fd9f5da7651b7351a7f580c0d96f68de7
Abolfazl Taghavi <taghavi.a2023@gmail.com>
2025-03-20 15:50:33 +0330

    config 🪓: add DRF app

---
+++ apps/django-app/authentication/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+  email = models.EmailField(unique=True)
+
+  # Add any additional fields here
+
+  def __str__(self):
+    return self.username
